d02b55a464408a98f34cad273e4dceca04be7777
sparker2000 <jtodd.cs@gmail.com>
2017-11-21 16:08:33 -0700

    First Commit

---
+++ CourseLister/Models/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CourseLister.Models
+{
+    public class HomeViewModel
+    {
+        public string CanvasAccountId { get; set; }
+        
+        public string Query { get; set; }
+        public bool Authorized { get; set; }
+    }
+}
---
+++ CourseLister/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CourseLister.Startup))]
+namespace CourseLister
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
